7fdc6c2f4b6a1ba3f57683effcf37e4b1dbc6001
William Laffin <william.laffin@gmail.com>
2024-11-26 21:42:49 -0500

    Create Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import io.vertx.core.Vertx;
+import io.vertx.core.net.SocketAddress;
+
+public class Main {
+    public static void main(String[] args) {
+        Vertx.vertx().createHttpServer().requestHandler(rc -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            rc.response().end("Service Response");
+        }).listen(8888);
+        Vertx.vertx().createHttpServer().requestHandler(rc -> rc.response().end("Service Ready")).listen(7999);
+        Runtime.getRuntime().addShutdownHook(new Thread("shutdown") {
+            public void run() {
+                System.out.println("sleeping before shutdown");
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            }
+        });
+    }
+}
